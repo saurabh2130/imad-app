51851c13404fc2f016f804e0683980951bbf9e3a
saurabh2130 <36417421+saurabh2130@users.noreply.github.com>
2018-03-22 21:07:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1',function(req,res){
+    res.send('Article 1 is requested');
+});
+app.get('/article1',function(req,res){
+    res.send('Article 2 is requested');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
