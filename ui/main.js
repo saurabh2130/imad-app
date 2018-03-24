d58c364cfa8b89f4647f412dc8b2d71bf6cdb5a9
saurabh2130 <36417421+saurabh2130@users.noreply.github.com>
2018-03-24 19:36:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 var img=document.getElementById('gg');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+4;
     img.style.marginLeft=marginLeft+'px'; 
 }
 img.onclick=function(){
-  var interval=setInterval(moveRight,100);
+  var interval=setInterval(moveRight,50);
    
 };
