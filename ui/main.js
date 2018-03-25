f127bc7f499dcdd0a7889389300fc2f1156ba010
saurabh2130 <36417421+saurabh2130@users.noreply.github.com>
2018-03-25 13:06:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,15 @@
-console.log('Loaded!');
-var img=document.getElementById('gg');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+4;
-    img.style.marginLeft=marginLeft+'px'; 
-}
-img.onclick=function(){
-  var interval=setInterval(moveRight,50);
-   
+var button=document.getElementById('counter');
+button.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }  
+      }
+  };
+  request.open('GET','http://saurabhakt30.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
