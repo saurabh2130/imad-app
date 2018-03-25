960f34ce00ca917e3f01770fc03ee6ef3ba66d55
saurabh2130 <36417421+saurabh2130@users.noreply.github.com>
2018-03-25 15:10:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,8 +13,7 @@ button.onclick=function(){
   request.open('GET','http://saurabhakt30.imad.hasura-app.io/counter',true);
   request.send(null);
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
    var request=new XMLHttpRequest();
@@ -32,6 +31,8 @@ submit.onclick=function(){
        ul.innerHTML=list;  
       }
   }
+  var nameInput=document.getElementById('name');
+  var name=nameInput.value;
   request.open('GET','http://saurabhakt30.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
 };
