7239b2d3e7d25d664abf531cada964be26804953
saurabh2130 <36417421+saurabh2130@users.noreply.github.com>
2018-03-25 12:51:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
+});
 
 app.get('/article1',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
